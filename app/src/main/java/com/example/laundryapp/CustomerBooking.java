c9bf07909a6f06cdfb3d8cddab02be2b815beef8
tiquiakt <tiquiakt@student.apc.edu.ph>
2022-06-13 21:56:27 +0800

    initial commit

--- app/src/main/java/com/example/laundryapp/CustomerBooking.java
+++ app/src/main/java/com/example/laundryapp/CustomerBooking.java
@@ -16,10 +16,88 @@ import java.util.Calendar;
 
 public class CustomerBooking extends AppCompatActivity {
 
+    private DatePickerDialog datePickerDialog;
+    private EditText dateButton, name, contact;
 
+    private Button bookNow, bttnHome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_booking);
+        initDatePicker();
+        dateButton.setText(getTodaysDate());
+
+        name = findViewById(R.id.editTxtFullname);
+        dateButton = findViewById(R.id.datePicker);
+
+
+        bttnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),customer_dashboard.class));
+                finish();
+            }
+        });
+    }
+    private String getTodaysDate(){
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        month = month + 1;
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        return makeDateString(day, month, year);
+    }
+    private void initDatePicker(){
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month +1;
+                String date = makeDateString(day, month, year);
+                dateButton.setText(date);
+            }
+        };
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        int style = AlertDialog.THEME_HOLO_LIGHT;
+        datePickerDialog = new DatePickerDialog(this, style, dateSetListener, year, month, day);
+
+    }
+    private String makeDateString(int day, int month, int year){
+        return getMonthFormat(month) + " " + day + " " + year;
+    }
+    private String getMonthFormat(int month){
+        if (month == 1)
+            return "January";
+        if (month == 2)
+            return "February";
+        if (month == 3)
+            return "March";
+        if (month == 4)
+            return "April";
+        if (month == 5)
+            return "May";
+        if (month == 6)
+            return "June";
+        if (month == 7)
+            return "July";
+        if (month == 8)
+            return "August";
+        if (month == 9)
+            return "September";
+        if (month == 10)
+            return "October";
+        if (month == 11)
+            return "November";
+        if (month == 12)
+            return "December";
+
+        return "January";
+    }
+    public void openDatePicker(View view){
+
+        datePickerDialog.show();
     }
 }
--- app/src/main/java/com/example/laundryapp/customer_dashboard.java
+++ app/src/main/java/com/example/laundryapp/customer_dashboard.java
@@ -38,15 +38,6 @@ public class customer_dashboard<uid> extends AppCompatActivity implements Naviga
     Toolbar toolbar;
     DrawerLayout drawerLayout;
     Button bttnBook, bttnPurchase;
-    private AlertDialog.Builder dialogBuilder;
-    private AlertDialog dialog;
-    private EditText name, dateButton;
-    private Button bookNow, bttnHome;
-    private DatePickerDialog datePickerDialog;
-
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,7 +62,8 @@ public class customer_dashboard<uid> extends AppCompatActivity implements Naviga
         bttnBook.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                createNewBookingDialog();
+                startActivity(new Intent(getApplicationContext(),CustomerBooking.class));
+                finish();
             }
         });
 
@@ -114,88 +106,5 @@ public class customer_dashboard<uid> extends AppCompatActivity implements Naviga
         }
         drawerLayout.closeDrawer(GravityCompat.START);
         return true;
-    }
-    public void createNewBookingDialog(){
-        initDatePicker();
-        dateButton.setText(getTodaysDate());
-        dialogBuilder = new AlertDialog.Builder(this);
-        final View bookingPopupView = getLayoutInflater().inflate(R.layout.activity_customer_booking, null);
-        name = (EditText) bookingPopupView.findViewById(R.id.editTxtFullname);
-        dateButton = (EditText) bookingPopupView.findViewById(R.id.datePicker);
-
-        bookNow = (Button) bookingPopupView.findViewById(R.id.bookbttn);
-        bttnHome = (Button) bookingPopupView.findViewById(R.id.homebttn);
-        dialogBuilder.setView(bookingPopupView);
-        dialog = dialogBuilder.create();
-        dialog.show();
-
-        bttnHome.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(),customer_dashboard.class));
-                finish();
-            }
-        });
-    }
-    private String getTodaysDate(){
-        Calendar cal = Calendar.getInstance();
-        int year = cal.get(Calendar.YEAR);
-        int month = cal.get(Calendar.MONTH);
-        month = month + 1;
-        int day = cal.get(Calendar.DAY_OF_MONTH);
-        return makeDateString(day, month, year);
-    }
-    private void initDatePicker(){
-        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-                month = month +1;
-                String date = makeDateString(day, month, year);
-                dateButton.setText(date);
-            }
-        };
-        Calendar cal = Calendar.getInstance();
-        int year = cal.get(Calendar.YEAR);
-        int month = cal.get(Calendar.MONTH);
-        int day = cal.get(Calendar.DAY_OF_MONTH);
-
-        int style = android.app.AlertDialog.THEME_HOLO_LIGHT;
-        datePickerDialog = new DatePickerDialog(this, style, dateSetListener, year, month, day);
-
-    }
-    private String makeDateString(int day, int month, int year){
-        return getMonthFormat(month) + " " + day + " " + year;
-    }
-    private String getMonthFormat(int month){
-        if (month == 1)
-            return "January";
-        if (month == 2)
-            return "February";
-        if (month == 3)
-            return "March";
-        if (month == 4)
-            return "April";
-        if (month == 5)
-            return "May";
-        if (month == 6)
-            return "June";
-        if (month == 7)
-            return "July";
-        if (month == 8)
-            return "August";
-        if (month == 9)
-            return "September";
-        if (month == 10)
-            return "October";
-        if (month == 11)
-            return "November";
-        if (month == 12)
-            return "December";
-
-        return "January";
-    }
-    public void openDatePicker(View view){
-
-        datePickerDialog.show();
     }
 }
